a0390e741892eff16133f57c9f1dfda9025b3582
thuong.le <thuongle24@gmail.com>
2015-08-31 09:46:41 +0700

    Delete folder Hai, Khuong, Thuong

--- Hai/actionBuiltIn/test.java
+++
@@ -1,5 +0,0 @@
-package actionBuiltIn;
-
-public class test {
-
-}
--- Khuong/objectRepository/LoginPage.java
+++
@@ -1,9 +0,0 @@
-package objectRepository;
-import org.openqa.selenium.By;
-
-public class LoginPage {
-	public By reporsitory_combobox=By.id("repository");
-	public By username_textbox=By.xpath("//input[@id='username']");
-	public By password_textbox= By.id("password");
-	public By login_button= By.cssSelector(".btn-login");
-}
