0a5df2ee2223b25a7d69615d7aa5469ca4c0d388
khuong.truong <truongnguyen.khuong@gmail.com>
2015-09-01 09:56:43 +0700

    up

--- src/dashboard/object_repository/MainPage_Dashboard.java
+++ src/dashboard/object_repository/MainPage_Dashboard.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 public class MainPage_Dashboard {
 	public By logout_link=By.cssSelector("[href*='logout']");
 	public By add_page_button=By.cssSelector("[href*=AddPage]");
-	public By edit_page_button=By.cssSelector("[href*=EditPage]");
+	public By edit_page_button=By.cssSelector(".edit[href*=openAddPage]");
 	public By test_page_icon=By.linkText("test");
 	public By test_child_page_icon=By.linkText("test child");
 	public By another_test_page_icon=By.linkText("Another Test");
@@ -13,6 +13,7 @@ public class MainPage_Dashboard {
 	public By overview_page_icon=By.linkText("Overview");
 	public By setting_link= By.className("mn-setting");
 	public By administer_link=By.cssSelector("a[href='#Administer']");
+	public By welcome_link=By.cssSelector("a[href='#Welcome']");
 	public By choose_panel_button= By.id("btnChoosepanel");
 	public By create_new_panel_button= By.cssSelector("span[onclick*='Dashboard.openAddPanel']");
 	public By panels_link=By.cssSelector("a[href='panels.jsp']");
