3f436b99142b505f9e1be1b851f873e36cb72190
Khuong.Truong <truongnguyen.khuong@gmail.com>
2015-09-14 02:17:52 +0700

    up

--- src/action_built_in/GetTableCellValue.java
+++ src/action_built_in/GetTableCellValue.java
@@ -1,24 +1,30 @@
 package action_built_in;
 
-public class GetTableCellValue {
+import java.util.List;
 
-	public GetTableCellValue() {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GetTableCellValue extends AutoAction{
+
+	public GetTableCellValue(WebDriver driver, By control, int row, int column, String value) {
+		
 		String data_you_want_to_check = "your data in here";
 		boolean check_data_existed = false;
-		WebDriver driver = new ChromeDriver();
-
-		driver.get("yoururl");
-
+		
 		//you can find table by xpath or some option ,depend on your site.
-		WebElement table = driver.findElement(By.id('yourTableID'));
+		WebElement table = driver.findElement(control);
 		//get all tr inside table
 		List<WebElement> tr_table = table.findElements(By.tagName("tr"));
+		tr_table.indexOf(1);
 		//now we will loop
 		for(WebElement el : tr_table){
-		  List<WebElement> td_table = tr_table.findElements(By.tagName("td"));
+		  List<WebElement> td_table = el.findElements(By.tagName("td"));
 		  for(WebElement elm : td_table){
 		      String text_inside_td = elm.getText();
-		      if(text_inside_td.equalsIgnoreCase(data_you_want_to_check)){
+		      if(text_inside_td.equalsIgnoreCase(data_you_want_to_check))
+		      {
 		          check_data_existed = true;
 		          break;
 		      }
